be6868308897d4750c47c166c2f08e5a7f484858
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-11 14:15:38 -0500

    2nd task done

--- index.js
+++ index.js
@@ -47,8 +47,11 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
+function getFinals(/* code here */arr) {
    /* code here */
+   return arr.filter((team) => {
+       return team['Stage'] == 'Final'
+   })
 }
 
 
@@ -59,8 +62,13 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
+function getYears(/* code here */arr, cb) {
     /* code here */
+  let years = cb((arr)).map(item => {
+      return item.Year
+  })
+  return years
+
 }
 
 
